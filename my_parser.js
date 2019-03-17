569c6ed8a03554e117b0715385d6d379795bc8ef
sjmckinney <stevejmckinney@gmail.com>
2019-03-17 22:28:06 +0000

    Handled failed specs listed for retry as ts files Added to 'my_parser.js' to handle conversion of failed spec file from original e2e/*.spec.ts to dist/*.spec.js Added my_parser as ts file but can't get compiled my_parser.js file to work. Protractor-flake lib/parsers/index.js reports parse is not a function

---
+++ my_parser.js
@@ -0,0 +1,22 @@
+const context = String.raw `\[object Object\]|Object|Context|UserContext|Suite`;
+const source = String.raw `<anonymous>|it|beforeEach|afterEach|before|after`;
+const filepath = String.raw `(([A-Za-z]:\\)?.*?):.*`;
+const regexString = String.raw `at (?:${context})\.(?:${source}) \(${filepath}\)`;
+
+module.exports = {
+    name: 'my_parser',
+    parse(output) {
+        let failedSpecs = new Set();
+        let match = null;
+        let FAILED_LINES = new RegExp(regexString, 'g');
+        //console.info(`BEGIN OUTPUT: ${output} END OUTPUT`);
+        while (match = FAILED_LINES.exec(output)) { // eslint-disable-line no-cond-assign
+            // windows output includes stack traces from
+            // webdriver so we filter those out here
+            if (!/node_modules/.test(match[1])) {
+                failedSpecs.add(match[1].replace('e2e', 'dist').replace('spec.ts', 'spec.js'));
+            }
+        }
+        return [...failedSpecs];
+    }
+}
